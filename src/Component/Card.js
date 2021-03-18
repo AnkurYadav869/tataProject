6f8beb2e85204344376b7145bfb9c4b828a75fb0
Ankur Yadav <ankuryadav869@gmail.com>
2021-03-18 16:34:34 +0530

    footer

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 function Card({ img, text }) {
 	return (
-		<div class='card zoom m-4 shadow-lg'>
+		<div className='card zoom m-4 shadow-lg'>
 			<div className='p-3'>
-				<img src={img} class='card-img-top' alt='...' />
+				<img src={img} className='card-img-top' alt='...' />
 			</div>
-			<div class='card-head p-3'>
-				<p class=' lead card-text text-secondary'>{text}</p>
+			<div className='card-head p-3'>
+				<p className=' lead card-text text-secondary'>{text}</p>
 			</div>
 		</div>
 	);
--- src/Component/Foot.js
+++ src/Component/Foot.js
@@ -22,33 +22,45 @@ function Foot() {
 				<div className='col-sm-2 my-3'>
 					<b className='h4 text-white'>PV Products</b>
 					<br />
-					<a href='' className='text-dark'>
+					<a href='http://photonsolar.in/pv-modules.php' className='text-dark'>
 						<br />
 						PV Modules
 					</a>
-					<a href='' className='text-dark'>
+					<a
+						href='http://photonsolar.in/smartsystems.php'
+						className='text-dark'
+					>
 						<br />
 						Solar Smart Systems
 					</a>
 				</div>
 				<div className='col-md-2 my-3'>
 					<b className='h4 text-white'>Solar Thermal</b> <br />
-					<a href='' className='text-dark'>
+					<a href='http://photonsolar.in/flat-plate.php' className='text-dark'>
 						{' '}
 						<br />
 						Flat Plate Collectors
 					</a>
-					<a href='' className='text-dark'>
+					<a
+						href='http://photonsolar.in/evacuated-tube.php'
+						className='text-dark'
+					>
 						{' '}
 						<br />
 						Evacuated Tube Collectors
 					</a>
-					<a href='' className='text-dark'>
+					<a
+						href='http://photonsolar.in/industrial-heating.php'
+						className='text-dark'
+					>
 						{' '}
 						<br />
 						Industrial Heating
 					</a>
-					<a href='' className='text-dark'>
+					<a
+						href='http://photonsolar.in/solar-desalination.php'
+						className='text-dark'
+					>
 						{' '}
 						<br />
 						Solar Desalination
@@ -57,39 +69,45 @@ function Foot() {
 				<div className='col-md-3 my-3'>
 					<b className='h4 text-white'>Solutions</b>
 					<br />
-					<a href='' className='text-dark'>
+					<a href='http://photonsolar.in/rooftop.php' className='text-dark'>
 						<br />
 						Solar Rooftops
 					</a>
 
-					<a href='' className='text-dark'>
+					<a href='http://photonsolar.in/megawatt.php' className='text-dark'>
 						<br />
 						Megawatt Power Plants
 					</a>
-					<a href='' className='text-dark'>
+					<a href='http://photonsolar.in/trackers.php' className='text-dark'>
 						<br />
 						Photon Trackers
 					</a>
 				</div>
 				<div className='col-md-3 my-3'>
 					<b className='h4 text-white'>Contact US</b> <br />
-					<a href='' className='text-dark'>
+					<a href='http://photonsolar.in/contactform.php' className='text-dark'>
 						<br />
 						Contact Form
 					</a>
-					<a href='' className='text-dark'>
+					<a
+						href='http://photonsolar.in/newdealership.php'
+						className='text-dark'
+					>
 						<br />
 						New Dealership
 					</a>
-					<a href='' className='text-dark'>
+					<a
+						href='http://photonsolar.in/complaintform.php'
+						className='text-dark'
+					>
 						<br />
 						Complaint Form
 					</a>
-					<a href='' className='text-dark'>
+					<a href='http://photonsolar.in/marketing.php' className='text-dark'>
 						<br />
 						Marketing and Dealers Network
 					</a>
-					<a href='' className='text-dark'>
+					<a href='http://photonsolar.in/sitemap.php' className='text-dark'>
 						<br />
 						Sitemap
 					</a>
